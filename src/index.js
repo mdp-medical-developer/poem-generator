553b4a44ed6b7eccdcc6fad5446c3391e1314257
mdp-medical-developer <info@mdp-medical.com>
2025-09-30 17:25:02 +0100

    Added basic HTML structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const KEY = "bf84c5dt8ba6f1571of07a1c8e407cf3";
-let context = "Be concise but funny";
-let prompt = "Tell me a joke";
+let context = "A four-line English poem with seven syllables per line";
+let prompt = "Generate ";
 const URL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${KEY}`;
