d05434c86d3c8f91690ff6ca7a84be247c9df35d
mdp-medical-developer <info@mdp-medical.com>
2025-09-30 16:34:18 +0100

    Initial set up

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const KEY = "bf84c5dt8ba6f1571of07a1c8e407cf3";
+let context = "Be concise but funny";
+let prompt = "Tell me a joke";
+const URL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${KEY}`;
