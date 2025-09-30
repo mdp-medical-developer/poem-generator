4fd97c3b60a582aef03e8b7b0a7624265859a688
mdp-medical-developer <info@mdp-medical.com>
2025-09-30 18:52:19 +0100

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-const KEY = "bf84c5dt8ba6f1571of07a1c8e407cf3";
-let context = "A four-line English poem with seven syllables per line";
-let prompt = "Generate ";
-const URL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${KEY}`;
-
+function displayPoem(response) {
+  let poemEl = document.querySelector("#poem");
+  poemEl.innerHTML = "";
+  let tw = new Typewriter("#poem", {
+    strings: response.data.answer,
+    autostart: true,
+    delay: 40,
+  });
+  tw.typeString(response.data.answer).start();
+}
 function generatePoem(event) {
   event.preventDefault();
-  new Typewriter("#poem", {
-    strings: "Sunlight warms the soft grass"
-    autostart: true,
-    dealy: 1,
-    cursor: "",}
-      );}
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+  let instructionsInput = document.querySelector("#user-instructions");
+  let poemEl = document.querySelector("#poem");
+  poemEl.innerHTML = `<em>Your poem is generating...</em>`;
+
+  /// ==================== API Info ===========================================
+  const KEY = "bf84c5dt8ba6f1571of07a1c8e407cf3";
+  let context =
+    "You are a romantic poet and love to write short poems. Your mission is to generate an English 4-line and 7-syllable-per-line poem and seperate each line with a <br .>. Make sure you follow the user instructions below. Do not include a title to the poem.";
+  let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
+  const URL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${encodeURIComponent(context)}&key=${encodeURIComponent(KEY)}`;
+
+  /// ===================== Fetch data ==========================================
+  axios.get(URL).then(displayPoem);
+}
+
+let poemFormEl = document.querySelector("#poem-generator-form");
+poemFormEl.addEventListener("submit", generatePoem);
